3f42bb87e7404fd33de07d4e6c4bc20df5706e8a
olamilekan-adeleke <43634204+Ola66@users.noreply.github.com>
2021-02-15 15:57:22 +0100

    sign up page ui

--- lib/ui/authentication/pages/sign_up_page.dart
+++ lib/ui/authentication/pages/sign_up_page.dart
@@ -1,3 +1,4 @@
+import 'package:blue_fibre/ui/authentication/widgets/sign_up_widget.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -7,13 +8,26 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).primaryColor,
+        body: body(context),
+      ),
+    );
+  }
+
+  Widget body(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
         children: <Widget>[
-          const Text('SignUp Page'),
-          FlatButton(
-            onPressed: () => callBack(),
-            child: const Text('next'),
+          SignUpPageMessage(size: size),
+          SignUpPageForm(size: size),
+          SignUpPageMoreOptions(size: size, navigate: () => callBack()),
+          SizedBox(
+            height: size.height * 0.10,
+            child: Container(color: Colors.white),
           ),
         ],
       ),
---
+++ lib/ui/authentication/widgets/sign_up_widget.dart
@@ -0,0 +1,172 @@
+import 'package:blue_fibre/business_logic/auth/repo/authentication_repo.dart';
+import 'package:blue_fibre/ui/home/pages/home_page.dart';
+import 'package:blue_fibre/ui/shared_widgets/custom_constant_widget.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPageMessage extends StatelessWidget {
+  const SignUpPageMessage({this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size.width,
+      height: size.height * 0.30,
+      padding: const EdgeInsets.only(left: 10.0, top: 20.0),
+      child: const CustomText(
+        text: 'Hello\nSign-Up To\nGet Started',
+        size: 35.0,
+        fontWeight: FontWeight.w300,
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+class SignUpPageForm extends StatefulWidget {
+  const SignUpPageForm({this.size});
+
+  final Size size;
+
+  @override
+  _SignUpPageFormState createState() => _SignUpPageFormState();
+}
+
+class _SignUpPageFormState extends State<SignUpPageForm> {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController fullNameController = TextEditingController();
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
+
+  Future<void> validate() async {
+    final FormState formState = formKey.currentState;
+
+    if (formState.validate()) {
+      final String email = emailController.text.trim();
+      final String password = passwordController.text.trim();
+
+      isLoading.value = true;
+      final AuthenticationRepo auth = AuthenticationRepo();
+
+      try {
+        // await auth.loginWithEmailAndPassword(email: email, password: password);
+        debugPrint('SignUp successfull');
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute<Widget>(
+            builder: (_) => HomePage(),
+          ),
+        );
+      } catch (e) {
+        debugPrint(e.toString());
+      }
+
+      isLoading.value = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: widget.size.width,
+      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(15.0),
+          topRight: Radius.circular(15.0),
+        ),
+      ),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const SizedBox(height: 30.0),
+            CustomTextField(
+              controller: fullNameController,
+              title: 'Full Name',
+              length: 3,
+            ),
+            const SizedBox(height: 15.0),
+            CustomTextField(
+              controller: userNameController,
+              title: 'User Name',
+              hideText: true,
+              length: 3,
+            ),
+            const SizedBox(height: 15.0),
+            CustomTextField(
+              controller: emailController,
+              title: 'Email Address',
+              hideText: true,
+            ),
+            const SizedBox(height: 15.0),
+            CustomTextField(
+              controller: phoneController,
+              title: 'Phone Number',
+              hideText: true,
+              length: 10,
+            ),
+            const SizedBox(height: 15.0),
+            CustomTextField(
+              controller: passwordController,
+              title: 'Password',
+              hideText: true,
+              length: 6,
+            ),
+            const SizedBox(height: 15.0),
+            ValueListenableBuilder<bool>(
+              valueListenable: isLoading,
+              builder: (_, bool value, Widget child) {
+                if (value == true) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return CustomButton(
+                    size: MediaQuery.of(context).size,
+                    title: 'SignUp',
+                    onPress: () => validate(),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignUpPageMoreOptions extends StatelessWidget {
+  const SignUpPageMoreOptions({this.size, this.navigate});
+
+  final Size size;
+  final Function navigate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      width: size.width,
+      height: size.height * 0.05,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          InkWell(
+            onTap: () => navigate(),
+            child: const CustomText(
+              text: 'Already Have An account? Sign In',
+              size: 16,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
